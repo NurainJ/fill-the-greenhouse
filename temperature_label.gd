d99f369bcb686d228e88a8c646a05673d46889bb
francesmcconnell <fmcconnellc@gmail.com>
2024-03-30 23:07:55 -0500

    Thermometer animation

--- temperature_label.gd
+++ temperature_label.gd
@@ -1,11 +1,21 @@
 extends Label
 
-
+var min_temp = 32
+var max_temp = 99
+var min_pos_y = 325 # Lowest positioj
+var max_pos_y = -5 # Highest position
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	set_text(str(GameVariables.temperature))
+	update_thermometer()
 
+func update_thermometer():
+	var temp_fraction = float(GameVariables.temperature - min_temp) / (max_temp - min_temp)
+	var thermometer_fill_node = get_node("../ThermometerMoving")
+	var new_pos_y = min_pos_y + temp_fraction * (max_pos_y - min_pos_y)
+	thermometer_fill_node.position.y = new_pos_y
+	set_text(str(GameVariables.temperature))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -15,12 +25,13 @@ func increase_temp():
 	if GameVariables.temperature < 99:
 		GameVariables.temperature += 1
 		set_text(str(GameVariables.temperature))
-
+		update_thermometer()
 
 func decrease_temp():
 	if GameVariables.temperature > 32:
 		GameVariables.temperature -= 1
 		set_text(str(GameVariables.temperature))
+		update_thermometer()
 	
 
 func _on_up_button_input_event(viewport, event, shape_idx):
