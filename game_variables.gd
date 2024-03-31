3c83cc0fbdf2f3ab78fd9b17c015e2b3244c3bd0
NurainJ <109047209+NurainJ@users.noreply.github.com>
2024-03-30 22:50:38 -0500

    Updated soil bag and starting plant
    The soil bag is two parts to aid layered animation. The game starts with empty pots, not seeds in pots.

--- game_variables.gd
+++ game_variables.gd
@@ -20,6 +20,7 @@ var soilPaths = Array()
 var possibleSoilPaths = ["res://art/pot/potSoilEmpty.svg", "res://art/pot/potSoilFull.svg", "res://art/pot/potSoilOverfull1.svg", "res://art/pot/potSoilOverfull2.svg"]
 
 var stateMachineData = {Species.LadySlipper: {
+						 "ladySlipperBlank": {"ladySlipper0": 0.5}, 
 							 "ladySlipper0": {"ladySlipper1": 0.5,                            "ladySlipperDead": 0.01}, 
 							 "ladySlipper1": {"ladySlipper2": 0.5, "ladySlipper1Dying": 0.25, "ladySlipperDead": 0.01}, 
 							 "ladySlipper2": {"ladySlipper3": 0.5, "ladySlipper2Dying": 0.25, "ladySlipperDead": 0.01}, 
@@ -31,6 +32,7 @@ var stateMachineData = {Species.LadySlipper: {
 						"ladySlipper4Dying": {"ladySlipper4": 0.5,                            "ladySlipperDead": 0.01}, 
 						  "ladySlipperDead": {}}, 
 						Species.Pasque: {
+						 "pasqueBlank": {"pasque0": 0.5},
 							 "pasque0": {"pasque1": 0.5,                       "pasqueDead": 0.01}, 
 							 "pasque1": {"pasque2": 0.5, "pasque1Dying": 0.25, "pasqueDead": 0.01}, 
 							 "pasque2": {"pasque3": 0.5, "pasque2Dying": 0.25, "pasqueDead": 0.01}, 
@@ -42,6 +44,7 @@ var stateMachineData = {Species.LadySlipper: {
 						"pasque4Dying": {"pasque4": 0.5,                       "pasqueDead": 0.01}, 
 						  "pasqueDead": {}},
 						Species.WildYam: {
+						 "wildYamBlank": {"wildYam0": 0.5}, 
 							 "wildYam0": {"wildYam1": 0.5,                        "wildYamDead": 0.01}, 
 							 "wildYam1": {"wildYam2": 0.5, "wildYam1Dying": 0.25, "wildYamDead": 0.01}, 
 							 "wildYam2": {"wildYam3": 0.5, "wildYam2Dying": 0.25, "wildYamDead": 0.01}, 
@@ -69,9 +72,9 @@ func _ready():
 				var state = possiblePlantStates[stateName]
 				var nextState = possiblePlantStates[nextStateName]
 				state.add_next_state(nextState, stateMachineData[species][stateName][nextStateName])
-	ladySlipperInitialState = possiblePlantStates["ladySlipper0"]
-	pasqueInitialState = possiblePlantStates["pasque0"]
-	wildYamInitialState = possiblePlantStates["wildYam0"]
+	ladySlipperInitialState = possiblePlantStates["ladySlipperBlank"]
+	pasqueInitialState = possiblePlantStates["pasqueBlank"]
+	wildYamInitialState = possiblePlantStates["wildYamBlank"]
 
 func _process(_delta):
 	pass
