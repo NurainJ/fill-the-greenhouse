91eb42d49ab20a3288b333cb74ee3557cf5e5433
jmpottsy22 <112656052+jmpottsy22@users.noreply.github.com>
2024-04-12 10:33:11 -0500

    adjusting factors before analysis

--- game_variables.gd
+++ game_variables.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var temperature = 75
-var days_remaining = 20
+var days_remaining = 40
 var activePlant = 0
 
 var plantStateClass = load("res://plant_state.gd")
@@ -24,10 +24,10 @@ var currentPacket: Species
 var stateMachineData = {Species.LadySlipper: {
 						 "ladySlipperBlank": {}, 
 							 "ladySlipper0": {"ladySlipper1": 0.42,                            "ladySlipperDead": 0.01}, 
-							 "ladySlipper1": {"ladySlipper2": 0.3, "ladySlipper1Dying": 0.2, "ladySlipperDead": 0.01}, 
-							 "ladySlipper2": {"ladySlipper3": 0.15, "ladySlipper2Dying": 0.10, "ladySlipperDead": 0.01}, 
-							 "ladySlipper3": {"ladySlipper4": 0.05, "ladySlipper3Dying": 0.03, "ladySlipperDead": 0.01}, 
-							 "ladySlipper4": {                     "ladySlipper4Dying": 0.1, "ladySlipperDead": 0.01}, 
+							 "ladySlipper1": {"ladySlipper2": 0.3, "ladySlipper1Dying": 0.2, "ladySlipperDead": 0.001}, 
+							 "ladySlipper2": {"ladySlipper3": 0.15, "ladySlipper2Dying": 0.10, "ladySlipperDead": 0.001}, 
+							 "ladySlipper3": {"ladySlipper4": 0.05, "ladySlipper3Dying": 0.03, "ladySlipperDead": 0.001}, 
+							 "ladySlipper4": {                     "ladySlipper4Dying": 0.1, "ladySlipperDead": 0.001}, 
 						"ladySlipper1Dying": {"ladySlipper1": 0.5,                            "ladySlipperDead": 0.01}, 
 						"ladySlipper2Dying": {"ladySlipper2": 0.5,                            "ladySlipperDead": 0.01}, 
 						"ladySlipper3Dying": {"ladySlipper3": 0.5,                            "ladySlipperDead": 0.01}, 
@@ -35,11 +35,11 @@ var stateMachineData = {Species.LadySlipper: {
 						  "ladySlipperDead": {}}, 
 						Species.Pasque: {
 						 "pasqueBlank": {},
-							 "pasque0": {"pasque1": 0.46,                       "pasqueDead": 0.01}, 
-							 "pasque1": {"pasque2": 0.35, "pasque1Dying": 0.25, "pasqueDead": 0.01}, 
-							 "pasque2": {"pasque3": 0.18, "pasque2Dying": 0.12, "pasqueDead": 0.01}, 
-							 "pasque3": {"pasque4": 0.06, "pasque3Dying": 0.04, "pasqueDead": 0.01}, 
-							 "pasque4": {                "pasque4Dying": 0.1, "pasqueDead": 0.01}, 
+							 "pasque0": {"pasque1": 0.46,                       "pasqueDead": 0.001}, 
+							 "pasque1": {"pasque2": 0.35, "pasque1Dying": 0.25, "pasqueDead": 0.001}, 
+							 "pasque2": {"pasque3": 0.18, "pasque2Dying": 0.12, "pasqueDead": 0.001}, 
+							 "pasque3": {"pasque4": 0.06, "pasque3Dying": 0.04, "pasqueDead": 0.001}, 
+							 "pasque4": {                "pasque4Dying": 0.1, "pasqueDead": 0.001}, 
 						"pasque1Dying": {"pasque1": 0.5,                       "pasqueDead": 0.01}, 
 						"pasque2Dying": {"pasque2": 0.5,                       "pasqueDead": 0.01}, 
 						"pasque3Dying": {"pasque3": 0.5,                       "pasqueDead": 0.01}, 
@@ -47,11 +47,11 @@ var stateMachineData = {Species.LadySlipper: {
 						  "pasqueDead": {}},
 						Species.WildYam: {
 						 "wildYamBlank": {}, 
-							 "wildYam0": {"wildYam1": 0.28,                        "wildYamDead": 0.01}, 
-							 "wildYam1": {"wildYam2": 0.2, "wildYam1Dying": 0.13, "wildYamDead": 0.01}, 
-							 "wildYam2": {"wildYam3": 0.11, "wildYam2Dying": 0.08, "wildYamDead": 0.01}, 
-							 "wildYam3": {"wildYam4": 0.03, "wildYam3Dying": 0.02, "wildYamDead": 0.01}, 
-							 "wildYam4": {                 "wildYam4Dying": 0.1, "wildYamDead": 0.01}, 
+							 "wildYam0": {"wildYam1": 0.28,                        "wildYamDead": 0.001}, 
+							 "wildYam1": {"wildYam2": 0.2, "wildYam1Dying": 0.13, "wildYamDead": 0.001}, 
+							 "wildYam2": {"wildYam3": 0.11, "wildYam2Dying": 0.08, "wildYamDead": 0.001}, 
+							 "wildYam3": {"wildYam4": 0.03, "wildYam3Dying": 0.02, "wildYamDead": 0.001}, 
+							 "wildYam4": {                 "wildYam4Dying": 0.1, "wildYamDead": 0.001}, 
 						"wildYam1Dying": {"wildYam1": 0.5,                        "wildYamDead": 0.01}, 
 						"wildYam2Dying": {"wildYam2": 0.5,                        "wildYamDead": 0.01}, 
 						"wildYam3Dying": {"wildYam3": 0.5,                        "wildYamDead": 0.01}, 
