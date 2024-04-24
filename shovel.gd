4e590b9bd243cac1cc64a8fa75af4dad6fe1396c
NurainJ <109047209+NurainJ@users.noreply.github.com>
2024-04-24 00:58:35 -0500

    Finished Shovel Animation

--- shovel.gd
+++ shovel.gd
@@ -26,7 +26,10 @@ func remove_soil():
 	tween.tween_property(self, "position", Vector2(1295, 473), 0.1)
 	tween.tween_property(self, "rotation", deg_to_rad(-34), 0.1)
 	await tween.finished
-	$AnimationPlayer.play("remove_soil")
+	if GameVariables.plantStates[GameVariables.activePlant] == GameVariables.initialStates[GameVariables.plantStates[GameVariables.activePlant].species]:
+		$AnimationPlayer.play("remove_soil")
+	else:
+		$AnimationPlayer.play("remove_plant")
 	await $AnimationPlayer.animation_finished
 	get_parent().isAnimating = false
 	
