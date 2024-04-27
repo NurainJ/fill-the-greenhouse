a299499ee0fef11e4708b0eac78e6c4f084a55ce
KellyLozanoAnalco <klozanoa@macalester.edu>
2024-04-27 12:47:28 -0500

    end screen progress so far

--- pot.gd
+++ pot.gd
@@ -17,7 +17,7 @@ var totalWater:int = 0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	get_parent().get_node("BlackScreen/BlackScreenPlayer").connect("is_black", _animation_finished.bind())
+	get_parent().get_node("/root/Root/EndScreen").connect("is_black", _animation_finished.bind())
 
 	
 
